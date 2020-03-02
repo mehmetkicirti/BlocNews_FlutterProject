f3f769db437c8fd3f244db65204bd2967c324369
mehmet <mehmetkicirti@hotmail.com>
2020-03-02 20:51:07 +0300

    To create clipper design has been created clipper class extends from CustomClipper

---
+++ lib/custom_shape_clipper.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+/*
+class CustomShapeClipper extends CustomClipper<Path>{
+  @override
+  getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0.0, size.height);
+    var firstEndPoint = Offset(size.width*0.5,size.height-35.0);
+    var firstControlPoint = Offset(size.width*0.25,size.height-50.0);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+    var secondEndPoint = Offset(size.width,size.height-80.0);
+    var secondControlPoint = Offset(size.width * 0.80, size.height-10);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
+
+}
+
+ */
+class CustomShapeClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    final Path _path = Path();
+    _path.lineTo(0, size.height*0.9);
+    var firstEndPoint = Offset(size.width,size.height*0.75);
+    var firstControlPoint = Offset(size.width*0.20,size.height-100);
+    _path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+    _path.lineTo(size.width, size.height);
+    _path.lineTo(size.width, 0);
+
+
+    _path.close();
+    return _path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+
+}
